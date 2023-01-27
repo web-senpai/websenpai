1248ffc74db843bbbd88a153693db3146e8f4262
samir <samir.uddin@enosisbd.com>
2023-01-27 21:05:02 +0600

    changed head

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,20 +15,12 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
-        <meta name="author" content="craftzdog" />
+        <meta name="description" content="Samir's homepage" />
+        <meta name="author" content="Samir shuvo" />
         <link rel="apple-touch-icon" href="websenpai.png" />
         <link rel="shortcut icon" href="/websenpai.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta name="og:title" content="Samir shuvo" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
         <title>Samir Shuvo - Portfolio</title>
       </Head>
 
